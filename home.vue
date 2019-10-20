a38940a9423fd98caed8ec7c52df691efcac0c77
huheng <947747958@qq.com>
2019-10-20 17:55:14 +0800

    '第一次提交home.vue'

---
+++ home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>hi TBD</div>
+    <p>h11111</p>
+    <p>home/1</p>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+
+</style>
