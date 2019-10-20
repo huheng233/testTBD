bf9610b0bc3ccbec8cae38363b4e5918cc3e58da
huheng <947747958@qq.com>
2019-10-20 18:45:22 +0800

    '第二次提交剩余的文件'

---
+++ home2.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>home222</p>
+    <p>home2/2</p>
+    <p>home2/3</p>
+    <p>home2/4</p>
+    <p>home2/5</p>
+</template>
+
+<script>
+    export default {
+        name: "home2"
+    }
+</script>
+
+<style scoped>
+
+</style>
